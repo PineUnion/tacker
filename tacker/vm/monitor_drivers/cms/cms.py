c796f3d2d45ea06410b11e549c16c87574bf6acd
PineUnion <doantunghust@gmail.com>
2016-02-11 22:38:52 -0800

    update class

---
+++ tacker/vm/monitor_drivers/cms/cms.py
@@ -0,0 +1,47 @@
+
+from oslo_config import cfg
+
+from tacker.agent.linux import utils as linux_utils
+from tacker.common import log
+from tacker.i18n import _LW
+from tacker.openstack.common import log as logging
+from tacker.vm.monitor_drivers import abstract_driver
+
+LOG = logging.getlogger(__name__)
+OPTS = [
+    cfg.StrOpt('count', default='1',
+               help=_('number of ICMP packets to send')),
+    cfg.StrOpt('timeout', default='1',
+               help=_('number of seconds to wait for a response')),
+    cfg.StrOpt('interval', default='1',
+               help=_('number of seconds to wait between packets'))
+]
+cfg.CONF.register_opts(OPTS, 'monitor_cms')
+
+
+class VNFMonitorCMS(abstract_driver.VNFMonitorAbstractDriver):
+       def get_type(self):
+           return 'cms'
+
+       def get_name(self):
+           return 'cms'
+
+       def get_description(self):
+           return 'Tacker VNFMonitor CMS Driver'
+
+       def monitor_url(self, plugin, context, device):
+           LOG.debug(_('monitor_url %s'), device)
+           return device.get('monitor_url', '')
+
+       def _is_cpu_overload(self, mgmt_cpu, thres):
+
+       def _is_mem_overload(self, mgmt_mem, thres):
+
+        @log.log
+        def monitor_call(self, device, kwargs):
+            if not kwargs['mgmt_cpu'] and kwargs['mgmt_mem']:
+                return
+            if kwargs['mgmt_cpu']:
+                return self._is_cpu_overload(**kwargs)
+            if kwargs['mgmt_mem']:
+                return self._is_mem_overload(**kwargs)
